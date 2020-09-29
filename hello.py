536f734140fb9bc13e18d2f470dab8a11c4aec7a
UTkzhang <kevinzhanghk2012@gmail.com>
2020-09-29 00:08:40 -0400

    activity code modifications completed, including new email field on form and html validator, flash message updates, etc

--- hello.py
+++ hello.py
@@ -3,22 +3,29 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'xyzyzxsomerandomstringthatisguesstohard'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(FlaskForm):
+class NameEmailForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()], render_kw={'type':'email'})
     submit = SubmitField('Submit')
 
 
+def UofT_email_check(field):
+    if "utoronto" not in field.data.split('@')[-1]:
+        return False
+    return True
+
 @app.errorhandler(404)
 def page_not_found(e):
+    session.clear()
     return render_template('404.html'), 404
 
 
@@ -26,14 +33,27 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+@app.route('/clear_session', methods=['GET'])
+def clear_session():
+    session.clear()
+    return "Session cleared"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = NameEmailForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
+        session['is_uoft_email'] = UofT_email_check(form.email)
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), is_uoft_email=session.get('is_uoft_email'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
